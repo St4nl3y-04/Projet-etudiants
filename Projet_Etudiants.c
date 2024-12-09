b3588ee64f8b12cf21816a49eab23f679480fee6
stanley <ismaillyamani4@gmail.com>
2024-12-09 03:53:48 +0100

    Working

--- Projet_Etudiants.c
+++ Projet_Etudiants.c
@@ -26,6 +26,34 @@ typedef struct ListeRepere{
     EtudiantRepere *queue;
     int nef;
 }Liste;
+Liste * Creer_Liste_Etudiants();
+float calculer_Moyenne(const EtudiantRepere *P);
+int calculerAge(const EtudiantRepere* P);
+void Libelle_notes(EtudiantRepere *P);
+void bultane_etu(EtudiantRepere*p);
+EtudiantRepere * Creer_Etudiant();
+void afficherEtudiant(EtudiantRepere* E);
+void ajouter_Etudiant_liste(Liste* li);
+void afficher_liste_Etudiant (Liste* li);
+void creer_fichier_txt ();
+void enregistrer_liste_etudiant(Liste* li);
+int* Recherche_et_Affichage_des_Informations_nom(Liste* li,int *nbt);
+int check_ID(int a);
+int Recherche_et_Affichage_des_Informations_identifiant(Liste* li);
+void afficher_menu_modifier(EtudiantRepere*p);
+void Rech_Pos_Occ(int* pos, const int nbt);
+void modifier_infor(Liste* li,int positions);
+int* Recherche_et_Affichage_des_Informations_age(Liste* li,int* nbt);
+Liste* lire_fichier_txt ();
+void afficher_menu();
+void afficher_menu_recherche();
+int Recherche_tous (Liste* liste);
+float* calculer_Moyenne_Module_Rapport(Liste* li);
+float calculer_Moyenne_Generale_Rapport(Liste* li);
+int Validation(Liste* li);
+void Generer_Rapport_Academique(Liste* li);
+void Suprimer_Etudiant(Liste* li, int pos);
+Liste* check_and_load_file();
 Liste * Creer_Liste_Etudiants(){
     Liste *Li;
     Li = (Liste*)malloc(sizeof(Liste));
@@ -107,15 +135,24 @@ void bultane_etu(EtudiantRepere*p){
     fprintf(file,"prenom:      %s \n",p->prenom);
     fprintf(file,"age:         %d \n",p->age);
     for(int i=0;i<NBR_NOTES;i++){
-        fprintf(file,"%s: %.2f \n",p->note[i].libelle,p->note[i].valeur);
+        fprintf(file,"%s: %.2f/20 \n",p->note[i].libelle,p->note[i].valeur);
     }
-    fprintf(file,"moyenne generale                               : %.2f \n",p->Moy);
+    fprintf(file,"moyenne generale                               : %.2f/20 \n",p->Moy);
+    if (p->Moy>=12){
+        fprintf(file,"Année validée.\n");
+    }else if (p->Moy<12){
+        fprintf(file,"Année non validée\n");
+    }
     fclose(file);
 }
 EtudiantRepere * Creer_Etudiant(){
     EtudiantRepere *P=(EtudiantRepere*)malloc(sizeof(EtudiantRepere));
-    printf("\nEntrez l'identifiant de l'etudiant :");
-    scanf(" %d", &P->Id);
+    int V=1;
+    do{
+        printf("\nEntrez l'identifiant de l'etudiant :");
+        scanf(" %d", &P->Id);
+        V=check_ID(P->Id);
+    }while (V==1);
     printf("Entrez le nom de l'etudiant :");
     scanf(" %[^\n]", P->nom);
     printf("Entrez le prenom de l'etudiant :");
@@ -238,6 +275,24 @@ int* Recherche_et_Affichage_des_Informations_nom(Liste* li,int *nbt) {
         }
 return pos;
 }
+int check_ID(int ID) {
+    Liste* li=check_and_load_file();
+    EtudiantRepere *courant=li->tete;
+    int found=0;
+    while(courant!=NULL){
+        if(courant-> Id==ID){
+            printf("L'ID que vous avez saisit existe deja dans la base de donnees, veuillez tapez un ID unique \n");
+            found=1;
+            break;
+            }
+        courant=courant->suivant;  
+    }
+    if(!found){
+        printf("L'ID est valide.\n");
+        return -1;
+    }
+    return 1;
+}
 int Recherche_et_Affichage_des_Informations_identifiant(Liste* li) {
     int ID;
     EtudiantRepere *courant=li->tete;
@@ -276,7 +331,9 @@ void afficher_menu_modifier(EtudiantRepere*p) {
     printf("19. Retour au menu principal\n");
     printf("Veuillez choisir une option : ");
 }
-void Rech_Pos_Occ(int *pos, const int nbt){
+
+void Rech_Pos_Occ(int* pos, const int nbt)
+{
     if(nbt != 0){
         printf("Nombre d'etudiants trouvée: %d", nbt);
         for(int i=0;i<nbt;i++){
@@ -284,11 +341,12 @@ void Rech_Pos_Occ(int *pos, const int nbt){
         }
     }
 }
-void* modifier_infor(Liste* li,int positions) {
+void modifier_infor(Liste *li, int positions)
+{
     int l;
     if (positions ==0 ) {
         printf("Étudiant non trouvé.\n");
-        return NULL;
+        return;
     }
 
     EtudiantRepere* courant = li->tete;
@@ -336,7 +394,7 @@ void* modifier_infor(Liste* li,int positions) {
         break;
         case 19:
                 printf("Retour au menu principal.\n");
-                return courant;
+                return;
             default:
                 if (l >= 5 && l < 5 + NBR_NOTES) {
                     int index = l - 5;
@@ -353,7 +411,7 @@ void* modifier_infor(Liste* li,int positions) {
     creer_fichier_txt ();
     enregistrer_liste_etudiant(li);
     bultane_etu(courant);
-    return courant;
+    return;
      }while (l!=19);
 }
 int* Recherche_et_Affichage_des_Informations_age(Liste* li,int* nbt) {
@@ -449,46 +507,44 @@ void afficher_menu_recherche(){
     printf("Choisissez une option: ");
 }
 int Recherche_tous (Liste* liste){
-            int R;
-            int nbt=0;
-            int position=0;
-            int* L=(int*)malloc(sizeof(int));
-            do
-            {
-                afficher_menu_recherche();
-                scanf("%d",&R);
-                nbt=0;
-                position=0;
-                switch (R)
-                {
-                    case 1:
-                    L=Recherche_et_Affichage_des_Informations_nom(liste,&nbt);
-                    if(L==NULL){
-                        return 0;
-                    }
-                    position=L[0];
-                    break;
-                    case 2:
-                    L=Recherche_et_Affichage_des_Informations_age(liste,&nbt);
-                    if(L==NULL){
-                        return 0;
-                    }
-                    position=L[0];
-                    break;
-                    case 3:
-                    position=Recherche_et_Affichage_des_Informations_identifiant(liste);
-                    if(position>1){
-                        nbt=1;
-                    }
-                    break;
-                    case 5:
-                    break;
-                    default:
-                    printf("Option invalide\n");
-                    break;
-                }
-            } while (R!=4 && nbt != 1);
-            return position;
+    int R;
+    int nbt=0;
+    int position=0;
+    int* L=(int*)malloc(sizeof(int));
+    do{
+        afficher_menu_recherche();
+        scanf("%d",&R);
+        nbt=0;
+        position=0;
+        switch (R){
+            case 1:
+            L=Recherche_et_Affichage_des_Informations_nom(liste,&nbt);
+            if(L==NULL){
+                return 0;
+            }
+            position=L[0];
+            break;
+            case 2:
+            L=Recherche_et_Affichage_des_Informations_age(liste,&nbt);
+            if(L==NULL){
+                return 0;
+            }
+            position=L[0];
+            break;
+            case 3:
+            position=Recherche_et_Affichage_des_Informations_identifiant(liste);
+            if(position>1){
+                nbt=1;
+            }
+            break;
+            case 5:
+            break;
+            default:
+            printf("Option invalide\n");
+            break;
+        }
+    } while (R!=4 && nbt != 1);
+    return position;
 }
 float* calculer_Moyenne_Module_Rapport(Liste* li){
     float *Moy=(float*)malloc(NBR_NOTES*(sizeof(float)));
@@ -518,16 +574,48 @@ float calculer_Moyenne_Generale_Rapport(Liste* li){
     MoyGen=S/li->nef;
     return MoyGen;
 }
+int Validation(Liste* li){
+    EtudiantRepere* courant=li->tete;
+    int EV=0;
+    while (courant != NULL){
+        if (courant->Moy>=12)
+        {
+            EV++;
+        }
+        courant=courant->suivant;
+    }
+    return EV;
+}
 void Generer_Rapport_Academique(Liste* li){
     FILE* fichier=fopen("Rapport_Academique.txt","w");
-    fprintf(fichier,"Le rapport académique de la filière GI1: \n");
+    fprintf(fichier,"<=================== Rapport Académique ===================>\n");
+    printf("<=================== Rapport Academique ===================>\n");
     EtudiantRepere P;
     Libelle_notes(&P);
     float* MoyMod=calculer_Moyenne_Module_Rapport(li);
     for (int i=0;i<NBR_NOTES;i++){
         fprintf(fichier,"%s : %.2f/20\n",P.note[i].libelle,MoyMod[i]);
+        printf("%s : %.2f/20\n", P.note[i].libelle, MoyMod[i]);
     }
-    fprintf(fichier,"La moyenne generale de la filière GI1           : %.2f/20\n", calculer_Moyenne_Generale_Rapport(li));
+    float B=calculer_Moyenne_Generale_Rapport(li);
+    fprintf(fichier,"La moyenne generale de la filière GI1           : %.2f/20\n", B);
+    printf("La moyenne generale de la filiere GI1           : %.2f/20\n", B);
+    fprintf(fichier,"Le nombre des étudiants dans la filière GI1           : %d\n", li->nef);
+    printf("Le nombre des etudiants dans la filiere GI1            : %d\n", li->nef);
+    int V=Validation(li);
+    fprintf(fichier,"Le nombre des étudiants qui ont validée l'année       : %d/%d\n",V,li->nef);
+    printf("Le nombre des etudiants qui ont validee l'annee       : %d/%d\n", V, li->nef);
+    fprintf(fichier,"Le nombre des étudiants qui n'ont pas validée l'année : %d/%d\n",li->nef-V,li->nef);
+    printf("Le nombre des etudiants qui n'ont pas validee l'annee : %d/%d\n",li->nef-V,li->nef);
+    if(V==li->nef){
+        fprintf(fichier,"Taux de validation: 100%%\n");
+        printf("Taux de validation: 100%%\n");
+    }else {
+        float T = ((float)V/li->nef)*100;
+        fprintf(fichier,"Taux de validation: %.2f%%\n",T);
+        printf("Taux de validation: %.2f%%\n",T);
+    }
+    printf("<================= Fin Rapport Academique =================>\n");
     fclose(fichier);
     free(MoyMod);
 }
